7a7a21e5e63a56e9ad60136dbcdcd1829027daae
Diana Grams <deegrams221@gmail.com>
2019-08-13 16:56:38 -0600

    installed axios dependency, and step 3 complete, except i'm still fixing an error i get: cant resolve axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React from "react";
+// imports
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import axios from "axios";
+import Header from "./components/Header";
+import APODCard from "./components/APODCard";
 
 function App() {
+  // state variables
+  const[photo, setPhoto] = useState("");
+  const[title, setTitle] = useState("");
+  const[info, setInfo] = useState("");
+  const[date, setDate] = useState("");
+
+  // effect hook
+  useEffect(() => {
+    axios
+      // calling Nasa API
+      .get(`https://api.nasa.gov/planetary/apod`)
+      .then(response => {
+        // states for each variable
+        const dailyAPOD = response.data.url;
+        console.log("daily APOD url: ", dailyAPOD);
+        setPhoto(dailyAPOD);
+
+        const dailyAPODTitle = response.data.title;
+        setTitle(dailyAPODTitle);
+
+        const dailyAPODInfo = response.data.explanation;
+        setInfo(dailyAPODInfo);
+
+        const dailyAPODDate = response.data.date;
+        setDate(dailyAPODDate);
+      })
+      // catch error message
+      .catch(error => {
+        //console.log(`API currently down: `, error);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
-      </p>
+      </p> */}
+      <Header date = {date} />
+      <APODCard dailyAPODURL = {photo} dailyAPODTitle = {title} explanation = {info} />
     </div>
   );
 }
